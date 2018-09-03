ecb023e483e29fef9162df79d2cc33a1a5b9d7f6
253008676@qq.com <253008676@qq.com>
2018-09-03 21:34:23 +0800

    陆续学习node之旅

---
+++ JS处理大数据.js
@@ -0,0 +1,19 @@
+a('2234728582639742973852836462365926397248738562352522','33373723749278372762763428347273875283497287357239472879523');
+function a(a,b){
+  let res = '';
+  let c = 0;
+  let d = 0;
+  a = a.split('');
+  b = b.split('');
+
+  while(a.length || b.length || c){
+    c += ~~a.pop() + ~~b.pop() + d;
+    d = parseInt(c / 10);
+    res = c % 10 + res;
+    c = 0;
+  }
+  if(d){
+    res = d + res;
+  }
+  console.log(res.replace(/^0+/,''));
+}
