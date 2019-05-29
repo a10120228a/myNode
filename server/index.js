1490c4fd72882fd2e6560d764aa19aac5b1f8fce
253008676@qq.com <253008676@qq.com>
2019-05-29 17:51:30 +0800

    express

--- server/index.js
+++ server/index.js
@@ -1,17 +1,9 @@
-const http = require('http');
-const fs = require('fs');
-
-http.createServer((req,res) => {
-  fs.readFile("../assert/liang.png",(err,data) => {
-    if(err){
-      throw err;
-    }
-    res.writeHead(200, {'Content-Type': 'image/png'});
-    res.write(data);
-    res.end();
-
-  });
-
-}).listen(8000,() => {
-  console.log('run at localhost:8000')
+const express = require('express');
+const app = express();
+app.get("/",(req,res) => {
+  res.send("hello world");
+  res.end();
+});
+app.listen(3000,() => {
+  console.log('node run at localhost:8000');
 });
