c1e57f23643923a715ec20cf729568f677c95248
253008676@qq.com <253008676@qq.com>
2019-06-09 13:09:24 +0800

    练习结束

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 app.get("/",(req,res) => {
-  res.send("hello world");
+  res.send("哈哈");
   res.end();
 });
 app.listen(3000,() => {
-  console.log('node run at localhost:8000');
+  console.log('node run at localhost:3000');
 });
+
+
